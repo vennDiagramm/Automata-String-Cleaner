5a32c058d6063b6509b356a76fd78e966c8fc3de
Darwin Guirigay <darwinlouis4@gmail.com>
2025-02-18 16:59:08 +0800

    Updated Version

--- cleaner.py
+++ cleaner.py
@@ -4,47 +4,50 @@ keywords = ["Buy Now", "Urgent", "Account", "Payment", "Verify", "Important", "h
 # The Input
 #userInput = input("What is the pattern: ")
 userInput = """
-Claim your FREEBIE of 1GB for surfing valid for 7 days now! Simply load a total of P90 before 02/23/2025. REF#: FGRN15
+From: callcenter@bdo.com.ph
+"Dear customer, your recent transaction has been successfully processed."
 """
-userInput = ''.join(userInput.split()) 
-
-# Verify sender identity
-def senderVerifier():
-    for sender in verifiedSenders:
-        if sender in userInput:
-            checkKeywords()
-            return "legit"
-    return "scammer"
+result = "" 
 
 # To avoid loop of senderVerifier and checkKeywords na functions
 attemptsToVerify = 0
 
+# Verify sender identity
+def senderVerifier():
+    global result
+    if any(sender in userInput for sender in verifiedSenders) and attemptsToVerify == 0:
+        result += "legit"
+        checkKeywords()
+    elif attemptsToVerify == 1:
+        result += "true"
+        checkDomain()
+    else:
+        result += "false"
+
 # If naay potential phishing words sa input
 def checkKeywords():
     global attemptsToVerify
+    global result
+    
     for phishingWord in keywords:
         if phishingWord in userInput:
             if attemptsToVerify == 0:
                 attemptsToVerify += 1
+                result += ''.join(phishingWord.lower().split())
                 senderVerifier()
-            else:
-                checkDomain()
-    return "link"
+    if attemptsToVerify == 0:
+        result += "link"
 
 # Check if domain is valid
 def checkDomain():
-    if userInput.startswith("https://"):
-        return True
-    elif userInput.startswith("http://"):
-        return False
-
+    global result
+    if "https://" in userInput:
+        result += "https://"
+    elif "http://" in userInput:
+        result += "http://"
 
 # Check the status sa conditions
 senderIdentity = senderVerifier()
 senderStatus = senderIdentity != "scammer"
-domainStatus = checkDomain()
 
-if not senderStatus or not domainStatus:
-    print("Phishing Detected")
-else:
-    print("No Phishing Detected")
+print(result)
